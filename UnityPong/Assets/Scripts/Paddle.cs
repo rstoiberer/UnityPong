7c9820e9ac5266027b2159d19051d7848981ba6e
Richard Stoiberer <rstoiberer@rollins.edu>
2025-09-13 16:23:19 -0400

    Commit Player Input
    We have handled player input so you can control paddles for 2 players

--- UnityPong/Assets/Scripts/Paddle.cs
+++ UnityPong/Assets/Scripts/Paddle.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Rigidbody2D rb2d;
+    public int id;
+    public float moveSpeed = 2f;
+
+    private void Awake()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();  
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float movement = ProcessInput();
+        Move(movement);
+    }
+
+    private float ProcessInput() 
+    {
+        float movement = 0f;
+        switch (id)
+        {
+            case 1:
+                movement = Input.GetAxis("MovePlayer1");
+                break;
+            case 2:
+                movement = Input.GetAxis("MovePlayer2");
+                break;
+        }
+        return movement;
+    }
+
+    private void Move(float value)
+    {
+        Vector2 velo = rb2d.linearVelocity;
+        velo.y = moveSpeed * value;
+        rb2d.linearVelocity = velo;
     }
 }
