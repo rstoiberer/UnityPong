4cad784d112f1380ba225dacde287a7618b2e5a4
Richard Stoiberer <rstoiberer@rollins.edu>
2025-09-13 12:11:39 -0400

    Commit ScoreZone, Ball Reset
    ScoreZone implemented.
    Ball reset implemented

--- UnityPong/Assets/Scripts/Ball.cs
+++ UnityPong/Assets/Scripts/Ball.cs
@@ -5,6 +5,9 @@ public class Ball : MonoBehaviour
     public Rigidbody2D rb2d;
     public float maxInitialAngle = 0.67f;
     public float moveSpeed = 1f;
+    public float maxStartY = 4f;
+
+    private float startX = 0f;
 
     private void Start()
     {
@@ -13,8 +16,28 @@ public class Ball : MonoBehaviour
     private void InitialPush()
     {
         Vector2 dir = Vector2.left;
+
+        if (Random.value < 0.5f)
+            dir = Vector2.right;
+
         dir.y = Random.Range(-maxInitialAngle, maxInitialAngle);
         rb2d.linearVelocity = dir * moveSpeed;
     }
 
+    private void ResetBall()
+    {
+        float posY = Random.Range(-maxStartY, maxStartY);
+        Vector2 position = new Vector2(startX, posY);
+        transform.position = position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ScoreZone scoreZone = collision.GetComponent<ScoreZone>();
+        if(scoreZone)
+        {
+            ResetBall();
+            InitialPush();
+        }
+    }
 }
